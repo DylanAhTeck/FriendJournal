d39135a5fbcdb45e266b7d54b5eca8e0fff3fe01
Dylan  Ah Teck <dylanahteck@Dylans-MacBook-Pro.local>
2019-06-24 20:56:47 -0700

    added update contact route

--- routes/contacts.js
+++ routes/contacts.js
@@ -63,8 +63,38 @@ router.post(
 // @ route      PUT api/contacts/:id
 // @ desc       Update contact
 // @ access     Private
-router.put('/:id', (req, res) => {
-  res.send('Update contact');
+router.put('/:id', auth, async (req, res) => {
+  const { name, email, phone, type } = req.body;
+
+  //Build contact object
+  const contactFields = {};
+  if (name) contactFields.name = name;
+  if (email) contactFields.email = email;
+  if (phone) contactFields.phone = phone;
+  if (type) contactFields.type = type;
+
+  try {
+    let contact = await Contact.findById(req.params.id);
+
+    if (!contact) res.status(400).send('Contact not found');
+
+    //Make sure user owns contact
+    if (contact.user.toString() != req.user.id) {
+      res.status(401).json({ msg: 'Not authorized' });
+    }
+
+    contact = await Contact.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: contactFields
+      },
+      { new: true }
+    );
+    res.json(contact);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 // @ route      DELETE api/contacts/:id
